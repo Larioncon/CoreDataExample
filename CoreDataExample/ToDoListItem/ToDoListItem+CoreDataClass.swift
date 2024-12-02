d73f9870356734b6cd06d2fecbd7682cf5ab3ab6
larioncon <hilltom@protonmail.com>
2024-12-02 06:02:02 +0200

    feat: implemented CoreData-based To-Do List with CRUD functionality
    - Added `ViewController` with UITableView to display the To-Do list.
    - Integrated CoreData for persistent storage of list items.
    - Implemented `createItem`, `getAllItems`, `updateItem`, and `deleteItem` methods to handle CRUD operations with CoreData.
    - Added user interaction via UIAlertControllers for adding, editing, and deleting tasks.
    - Improved UI with dynamic UITableView updates and proper data handling.
    
    This implementation serves as the base for managing tasks with persistent storage.

---
+++ CoreDataExample/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataExample
+//
+//  Created by Chmil Oleksandr on 29.11.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ CoreDataExample/ToDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  CoreDataExample
+//
+//  Created by Chmil Oleksandr on 29.11.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
